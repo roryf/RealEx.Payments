b3be737e4a0af6ab2677d7d2902f9ede76208301
Rory Fitzpatrick <rory@stormid.com>
2012-06-15 12:04:34 +0100

    Adds MVC project with response model binder

---
+++ RealEx.Mvc/TransactionResponseModelBinder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace RealEx.Mvc
+{
+    public class TransactionResponseModelBinder : IModelBinder
+    {
+        const string AuthCode = "AUTHCODE";
+        const string CvnResult = "CVNRESULT";
+        const string MerchantId = "MERCHANT_ID";
+        const string RealExTransactionReference = "PASREF";
+        const string ResultCode = "RESULT";
+        const string ResultMessage = "MESSAGE";
+        const string Sha1Hash = "SHA1HASH";
+        const string SubAccount = "ACCOUNT";
+        const string Timestamp = "TIMESTAMP";
+        const string TransactionId = "ORDER_ID";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            return new TransactionResponse
+            {
+                AuthCode = GetFormField(AuthCode, bindingContext.ValueProvider),
+                CvnResult = GetFormField(CvnResult, bindingContext.ValueProvider),
+                MerchantId = GetFormField(MerchantId, bindingContext.ValueProvider),
+                RealExTransactionReference = GetFormField(RealExTransactionReference, bindingContext.ValueProvider),
+                ResultCode = Convert.ToInt32(GetFormField(ResultCode, bindingContext.ValueProvider)),
+                ResultMessage = GetFormField(ResultMessage, bindingContext.ValueProvider),
+                Sha1Hash = GetFormField(Sha1Hash, bindingContext.ValueProvider),
+                SubAccount = GetFormField(SubAccount, bindingContext.ValueProvider),
+                Timestamp = GetFormField(Timestamp, bindingContext.ValueProvider),
+                TransactionId = GetFormField(TransactionId, bindingContext.ValueProvider)
+            };
+        }
+
+        string GetFormField(string key, IValueProvider provider)
+        {
+            ValueProviderResult result = provider.GetValue(key);
+
+            if (result != null)
+            {
+                return (string)result.ConvertTo(typeof(string));
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/RealEx.Tests/Mvc/TransactionResponseModelBinderSpecs.cs
@@ -0,0 +1,64 @@
+﻿using System.Web.Mvc;
+using Machine.Specifications;
+using RealEx.Mvc;
+
+namespace RealEx.Tests.Mvc
+{
+    public class TransactionResponseModelBinderSpecs
+    {
+        [Subject(typeof(TransactionResponseModelBinder))]
+        public class when_binding_from_form_collection
+        {
+            private static TransactionResponseModelBinder binder;
+            private static ModelBindingContext bindingContext;
+            private static TransactionResponse result;
+
+            Establish context = () =>
+                {
+                    var postData = new FormCollection
+                    {
+                        {"AUTHCODE", "1234"},
+                        {"CVNRESULT", "M"},
+                        {"MERCHANT_ID", "TestMerchant"},
+                        {"PASREF", "20036839SomeGUIDGoesHere"},
+                        {"RESULT", "00"},
+                        {"MESSAGE", "Successful"},
+                        {"SHA1HASH", "e34e77dd24d6b66dd6a6226149544806033f6600"},
+                        {"ACCOUNT", "test"},
+                        {"TIMESTAMP", "20120615120000"},
+                        {"ORDER_ID", "00000000-0000-0000-0000-000000000000"}
+                    };
+
+                    bindingContext = new ModelBindingContext
+                    {
+                        ModelMetadata = ModelMetadataProviders.Current.GetMetadataForType(null, typeof(TransactionResponse)),
+                        ValueProvider = postData.ToValueProvider()
+                    };
+
+                    binder = new TransactionResponseModelBinder();
+                };
+
+            Because of = () => result = (TransactionResponse)binder.BindModel(new ControllerContext(), bindingContext);
+
+            It should_set_merchant_id = () => result.MerchantId.ShouldEqual("TestMerchant");
+
+            It should_set_sub_account = () => result.SubAccount.ShouldEqual("test");
+
+            It should_set_transaction_id = () => result.TransactionId.ShouldEqual("00000000-0000-0000-0000-000000000000");
+
+            It should_set_timestamp = () => result.Timestamp.ShouldEqual("20120615120000");
+
+            It should_set_auth_code = () => result.AuthCode.ShouldEqual("1234");
+
+            It should_set_result_code = () => result.ResultCode.ShouldEqual(0);
+
+            It should_set_result_message = () => result.ResultMessage.ShouldEqual("Successful");
+
+            It should_set_cvn_result = () => result.CvnResult.ShouldEqual("M");
+
+            It should_set_realex_transaction_reference = () => result.RealExTransactionReference.ShouldEqual("20036839SomeGUIDGoesHere");
+
+            It should_set_sha1_hash = () => result.Sha1Hash.ShouldEqual("e34e77dd24d6b66dd6a6226149544806033f6600");
+        }
+    }
+}
